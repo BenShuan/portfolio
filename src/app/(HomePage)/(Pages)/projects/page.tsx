c4388bc6b21a2d83dd0999488c197eaad31c9c88
Ben Shuan <benshuan@gmail.com>
2024-11-17 18:19:18 +0200

    fix build issues

--- src/app/(HomePage)/(Pages)/projects/page.tsx
+++ src/app/(HomePage)/(Pages)/projects/page.tsx
@@ -8,7 +8,7 @@ function Projects() {
     <div className="flex flex-wrap justify-center h-full overflow-scroll py-14 gap-9 ">
       {projects.map((proj) => {
         return (
-         <Project proj={proj}/>
+         <Project key={proj.Id}  proj={proj}/>
         );
       })}
     </div>
